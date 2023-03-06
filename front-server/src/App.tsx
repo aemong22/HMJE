458cac866a69d1d735d71b900b5cf60d08c077e9
wndjf11 <wndjf11@naver.com>
2023-03-06 14:06:41 +0900

    Feat : 추가 state 연결

--- front-server/src/App.tsx
+++ front-server/src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
-import Intro from './Components/Intro/Intro';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Intro from "./Components/Intro/Intro";
+import Login from "./Components/Login/Login";
 
 function App() {
   const notify = () => toast("Wow so easy!");
   return (
     <>
       <Routes>
-        <Route path='/' element={<Intro/>}/>
+        <Route path="/" element={<Intro />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
--- front-server/src/Components/Login/Login.tsx
+++ front-server/src/Components/Login/Login.tsx
@@ -5,45 +5,49 @@ import Navbar from "../../Common/Navbar";
 const Login = () => {
   // 입력 타입
   type input = string | undefined;
-  const [Id, setId] = useState<input>();
-  const [Password, setPassword] = useState<input>();
+  const [Id, setId] = useState();
+  const [Password, setPassword] = useState();
 
   const ChangeId = (event: any) => {
+    // console.log(event.target.value);
     setId(event.target.value);
   };
   const ChangePassword = (event: any) => {
+    // console.log(event.target.value);
     setPassword(event.target.value);
   };
   return (
     <>
       <div className="flex flex-col h-full w-full">
         <Navbar />
-        <Mainlogin />
+        <div className="flex flex-col items-center">
+          <div className="flex flex-col grow items-center justify-center border-2 h-[70rem] w-[80rem] border-black">
+            <div>홍민정음</div>
+            <div>즐거운 단어 학습</div>
+            <div>또이름</div>
+            <input
+              className="border-black border-2 rounded-[3rem]"
+              onChange={ChangeId}
+              placeholder="이름"
+            />
+            <div>비밀번호</div>
+            <input
+              className="border-black border-2 rounded-[3rem]"
+              onChange={ChangePassword}
+              placeholder="비밀번호"
+            />
+            <div className="cursor-pointer">입장하기</div>
+
+            <div className="flex felx-row">
+              <div className="cursor-pointer">가입하기</div>
+              <div className="cursor-pointer">또이름 찾기</div>
+              <div className="cursor-pointer">비밀번호 찾기</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
-
-  function Mainlogin(): JSX.Element {
-    return (
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col grow items-center justify-center border-2 h-[30rem] w-[30rem] border-black">
-          <div>또이름</div>
-          <input
-            className="border-black border-2"
-            onChange={ChangeId}
-            placeholder="이름"
-          />
-          <div>비밀번호</div>
-          <input
-            className="border-black border-2"
-            onChange={ChangePassword}
-            placeholder="비밀번호"
-          />
-          <div className="cursor-pointer">입장하기</div>
-        </div>
-      </div>
-    );
-  }
 };
 
 export default Login;
