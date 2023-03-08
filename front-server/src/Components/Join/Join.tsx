d6fb24b949f58c6c4b12f2187391fed18c262b5a
wndjf11 <wndjf11@naver.com>
2023-03-08 10:30:26 +0900

    Style : 추가 레이아웃 수정중

--- front-server/src/Components/Join/Join.tsx
+++ front-server/src/Components/Join/Join.tsx
@@ -1,32 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Footer from "../Common/Footer";
 import Navbar from "../Common/Navbar";
 
-const Join = (): JSX.Element => {
-  const [Name, setName] = useState<string>();
-  const [Password, setPassword] = useState<string>();
-  const [PasswordCheck, setPasswordCheck] = useState<string>();
-  const [Nickname, setNickname] = useState<string>();
-  const [Phonenum, setPhonenum] = useState<number>();
-  const [Authnum, setAuthnum] = useState<number>();
-  const [TextSizeTest, setTextSizeTest] = useState<number>(70);
+const Join = () => {
+  const [Name, setName] = useState();
+  const [Password, setPassword] = useState();
+  const [PasswordCheck, setPasswordCheck] = useState();
+  const [Nickname, setNickname] = useState();
+  const [Phonenum, setPhonenum] = useState<string>();
+  const [Authnum, setAuthnum] = useState<string>();
 
-  const TextSize = 30; //가입하기
-  const DefaultNum = TextSize;
-  const HongSize = (TextSize * 80) / DefaultNum; //50
-  const TitleSize = (TextSize * 25) / DefaultNum; //25
-  const AcceptSize = (TextSize * 20) / DefaultNum; //20
-  const ExplanationSize = (TextSize * 15) / DefaultNum; //15
-
-  useEffect(() => {
-    setTextSizeTest(70);
-    return () => {};
-  }, []);
-
-  const ChangeName = (event: any): void => {
+  function ChangeName(event: any): void {
     console.log(event.target.value);
     setName(event.target.value);
-  };
+  }
   const ChangePassword = (event: any): void => {
     console.log(event.target.value);
     setPassword(event.target.value);
@@ -43,8 +30,8 @@ const Join = (): JSX.Element => {
     console.log(event.target.value);
     setPhonenum(event.target.value);
   };
-  const ChangeAuthnum = (event: any): void => {
-    console.log(event.target.value);
+  const ChangeAuthnum = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    // console.log(event.target.value);
     setAuthnum(event.target.value);
   };
 
@@ -59,8 +46,9 @@ const Join = (): JSX.Element => {
   };
 
   const CheckAuthnum = (): void => {
-    if (Authnum === 1) {
-    }
+    // if (Authnum == 1) {
+    // } else {
+    // }
     // 인증번호확인axios
   };
 
@@ -68,148 +56,173 @@ const Join = (): JSX.Element => {
     // 회원가입axios
   };
 
-  console.log(ExplanationSize);
-
   return (
     <>
       <Navbar />
-      <div
-        id="container"
-        className="flex flex-col items-center justify-center h-[30rem] sm:h-[30rem] md:h-[40rem] lg:h-[50rem]"
-      >
-        <div id="head" className="h-[10rem]">
-          <div
-            className={`font-extrabold text-[#A87E6E] text-[${HongSize}px] 
-            sm:text-[${HongSize * 0.5}px] md:text-[${HongSize * 0.8}px] 
-            lg:text-[${HongSize}px]`}
-          >
-            홍민정음 {HongSize}
+      <div id="container" className="flex flex-col w-screen">
+        <div className="flex flex-col px-10">
+          <div className="text-3xl font-extrabold text-[#A87E6E] sm:text-3xl md:text-4xl lg:text-6xl">
+            홍민정음
           </div>
-          <div
-            className={`font-extrabold text-[#BD9789] text-[${TextSize}px] sm:text-[1rem] md:text-[1.6rem] lg:text-[${TextSize}px]`}
-          >
+          <div className="text-[#BD9789] font-extrabold text-xl sm:text-xl md:text-2xl lg:text-4xl ">
             가입하기
           </div>
         </div>
-        <div id="body" className="h-[30rem]">
-          <div
-            className={`font-extrabold text-[#A87E6E] text-[${TitleSize}px] sm:text-[1rem] md:text-[1.6rem] lg:text-[${TitleSize}px]`}
-          >
-            또이름testse
-            {TitleSize}px
-          </div>
-          <div className="flex flex-row">
-            <input
-              type="text"
-              className="border-2 border-[#A87E6E]"
-              onChange={ChangeName}
-            />
-            <div
-              className={`text-[${AcceptSize}px] sm:text-[0.8rem] md:text-[1.28rem] lg:text-[${AcceptSize}px]  border-2 border-black rounded-[15px] cursor-pointer`}
-              onClick={() => {
-                CheckDuplication("Name");
-              }}
-            >
-              중복확인
+        <div className=" flex flex-col justify-center max-w-[700px] mx-auto pt-4">
+          <div className="h-[4rem]">
+            <div className="text-[#A87C6E] font-extrabold text-base">
+              또이름
+            </div>
+            <div className="flex flex-row ">
+              <input
+                type="text"
+                className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-[#A87E6E] rounded-lg font-medium placeholder:font-normal"
+                onChange={ChangeName}
+              />
+              <div className="flex px-3 py-2 md:px-4 md:py-3 border-2 bg-[#BF9F91] text-[#FFFFFF]  rounded-lg font-medium">
+                중복확인
+              </div>
             </div>
           </div>
-          <div className="flex flex-row">
-            <div
-              className={`font-extrabold text-[#A87E6E] text-[${TitleSize}px] sm:text-[1rem] md:text-[1.6rem] lg:text-[${TitleSize}px] `}
-            >
-              별명
-              {TitleSize}rem
-            </div>
-            <div
-              className={`text-[#868686] text-[${ExplanationSize}px] sm:text-[1rem] md:text-[1.6rem] lg:text-[${ExplanationSize}px]`}
-            >
-              6자 이내 한글만 사용하실 수 있습니다. {ExplanationSize}px
-            </div>
-          </div>
-          <div className="flex flex-row">
-            <input
-              type="text"
-              className="border-2 border-[#A87E6E]"
-              onChange={ChangeNickname}
-            />
-            <div
-              className={`text-[${AcceptSize}px] sm:text-[0.8rem] md:text-[1.28rem] lg:text-[${AcceptSize}px] border-2 border-black rounded-[15px] cursor-pointer`}
-              onClick={() => {
-                CheckDuplication("Nickname");
-              }}
-            >
-              중복확인
+          <div className="h-[4rem]">
+            <div className="text-[#A87C6E] font-extrabold text-base">별명</div>
+            <div className="flex flex-row ">
+              <input
+                type="text"
+                className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-[#A87E6E] rounded-lg font-medium placeholder:font-normal"
+                onChange={ChangeName}
+              />
+              <div className="flex px-3 py-2 md:px-4 md:py-3 border-2 bg-[#BF9F91] text-[#FFFFFF]  rounded-lg font-medium">
+                중복확인
+              </div>
             </div>
           </div>
-          <div
-            className={`font-extrabold text-[#A87E6E] text-[${TitleSize}px] sm:text-[1rem] md:text-[1.6rem] lg:text-[${TitleSize}px]`}
-          >
-            비밀번호
-          </div>
-          <input
-            type="text"
-            className="border-2 border-[#A87E6E]"
-            onChange={ChangePassword}
-          />
-          <div
-            className={`font-extrabold text-[#A87E6E] text-[${TitleSize}px] sm:text-[1rem] md:text-[1.6rem] lg:text-[${TitleSize}px]`}
-          >
-            비밀번호 확인
-          </div>
-          <input
-            type="text"
-            className="border-2 border-[#A87E6E]"
-            onChange={ChangePasswordCheck}
-          />
-          <div
-            className={`font-extrabold text-[#A87E6E] text-[${TitleSize}px] sm:text-[1rem] md:text-[1.6rem] lg:text-[${TitleSize}px]`}
-          >
-            전화번호
-          </div>
-          <div className="flex flex-row">
-            <input
-              type="text"
-              className="border-2 border-[#A87E6E]"
-              onChange={ChangePhonenum}
-            />
-            <div
-              className={`text-[${AcceptSize}px] sm:text-[0.8rem] md:text-[1.28rem] lg:text-[${AcceptSize}px] border-2 border-black rounded-[15px] cursor-pointer`}
-              onClick={() => {
-                CheckDuplication("Phonenum");
-              }}
-            >
-              중복확인
+          <div className="h-[4rem]">
+            <div className="text-[#A87C6E] font-extrabold text-base">
+              비밀번호
+            </div>
+            <div className="flex flex-row ">
+              <input
+                type="text"
+                className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-[#A87E6E] rounded-lg font-medium placeholder:font-normal"
+                onChange={ChangeName}
+              />
             </div>
           </div>
-          <div className="flex flex-row">
-            <div
-              className={`text-[${AcceptSize}px] sm:text-[0.8rem] md:text-[1.28rem] lg:text-[${AcceptSize}px] border-2 border-black rounded-[15px]`}
-            >
-              인증번호 요청
+          <div className="h-[4rem]">
+            <div className="text-[#A87C6E] font-extrabold text-base">
+              비밀번호확인
             </div>
-            <input
-              type="text"
-              className="border-2 border-[#A87E6E]"
-              onChange={ChangeAuthnum}
-            />
-            <div
-              className={`text-[${AcceptSize}px] sm:text-[0.8rem] md:text-[1.28rem] lg:text-[${AcceptSize}px] border-2 border-black rounded-[15px] cursor-pointer`}
-              onClick={CheckAuthnum}
-            >
-              확인
+            <div className="flex flex-row ">
+              <input
+                type="text"
+                className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-[#A87E6E] rounded-lg font-medium placeholder:font-normal"
+                onChange={ChangeName}
+              />
             </div>
           </div>
-          <div
-            className={`border-2 border-[#A87E6E] font-extrabold text-[#BD9789] text-[${TextSize}px] sm:text-[1rem] md:text-[1.6rem] lg:text-[${TextSize}px] cursor-pointer`}
-            onClick={GoJoin}
-          >
-            가입하기
+          <div className="h-[4rem]">
+            <div className="text-[#A87C6E] font-extrabold text-base">
+              전화번호
+            </div>
+            <div className="flex flex-row ">
+              <input
+                type="text"
+                className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-[#A87E6E] rounded-lg font-medium placeholder:font-normal"
+                onChange={ChangeName}
+              />
+              <div className="flex px-3 py-2 sm:px-3 sm:py-2 md:px-4 md:py-3  border-2 bg-[#BF9F91] text-[#FFFFFF]  rounded-lg font-medium">
+                중복확인
+              </div>
+            </div>
+          </div>
+          <div className="h-[4rem]">
+            <div className="text-[#A87C6E] font-extrabold text-base">
+              인증번호
+            </div>
+            <div className="flex flex-row ">
+              <input
+                type="text"
+                className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-[#A87E6E] rounded-lg font-medium placeholder:font-normal"
+                onChange={ChangeName}
+              />
+              <div className="flex px-3 py-2 md:px-4 md:py-3 border-2 bg-[#BF9F91] text-[#FFFFFF]  rounded-lg font-medium">
+                확인
+              </div>
+            </div>
+          </div>
+          <div className="pt-3">
+            <div className="flex justify-center items-center h-8 rounded-lg font-extrabold bg-[#F0ECE9] text-[#A87E6E]">
+              <div>가입하기</div>
+            </div>
           </div>
         </div>
+        <div className="h-[2rem]"></div>
       </div>
+
       <Footer />
     </>
   );
 };
 
+{
+  /* <div className="flex flex-col items-center justify-center">
+  <div className="h-[10rem]">
+    <div>홍민정음</div>
+    <div>가입하기</div>
+  </div>
+  <div className="h-[30rem]">
+    <div>또이름</div>
+    <div className="flex flex-row">
+      <input
+        type="text"
+        className="border-2 border-black"
+        onChange={ChangeName}
+      />
+      <div>중복확인</div>
+    </div>
+    <div>별명</div>
+    <div className="flex flex-row">
+      <input
+        type="text"
+        className="border-2 border-black"
+        onChange={ChangeNickname}
+      />
+      <div>중복확인</div>
+    </div>
+    <div>비밀번호</div>
+    <input
+      type="text"
+      className="border-2 border-black"
+      onChange={ChangePassword}
+    />
+    <div>비밀번호 확인</div>
+    <input
+      type="text"
+      className="border-2 border-black"
+      onChange={ChangePasswordCheck}
+    />
+    <div>전화번호</div>
+    <div className="flex flex-row">
+      <input
+        type="text"
+        className="border-2 border-black"
+        onChange={ChangePhonenum}
+      />
+      <div>중복확인</div>
+    </div>
+    <div>인증번호</div>
+    <div className="flex flex-row">
+      <div>인증번호 요청</div>
+      <input
+        type="text"
+        className="border-2 border-black"
+        onChange={ChangeAuthnum}
+      />
+      <div>확인</div>
+    </div>
+    <div className="border-2 border-black">가입하기</div>
+  </div>
+</div>; */
+}
 export default Join;
