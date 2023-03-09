14e74a5e24051b3c01d6a8b03e79cbb1c48d3099
wndjf11 <wndjf11@naver.com>
2023-03-09 15:34:03 +0900

    Feat : 추가 로그인 및 중복 axios 연결

--- front-server/src/Components/Common/Api.tsx
+++ front-server/src/Components/Common/Api.tsx
@@ -2,11 +2,21 @@ import axios from "axios";
 import jwt from "jsonwebtoken";
 
 const Api = axios.create({
-  baseURL: "https://i8e102.p.ssafy.io",
+  baseURL: "http://118.67.130.158/api",
   headers: {
     accessToke: localStorage.getItem("accessToken"),
   },
 });
+
+// function decodeToken(token: string) {
+//   try {
+//     const decodedToken = jwt.verify(token, "your_secret_key");
+//     return decodedToken;
+//   } catch (err) {
+//     console.error(err);
+//     return null;
+//   }
+// }
 
 Api.interceptors.request.use(
   // request할때 할 행동
@@ -21,20 +31,20 @@ Api.interceptors.request.use(
     //토큰이 있으면
     if (accessToken != null) {
       // 유효성판단
-      console.log(accessToken);
+      console.log("accessToken있다", accessToken);
 
-      const decode: jwt.JwtPayload | null = jwt.decode(accessToken, {
-        complete: true,
-      });
-      const nowDate: number = new Date().getTime() / 1000;
-      // 토큰 만료시간이 지났다면
-      if (decode?.exp != undefined) {
-        if (decode.exp < nowDate) {
-          // 재발급 axios ㄱㄱ
-          // 리프레쉬 토큰 발급 서버 요청
-          // accessToken = refreshToken;
-        }
-      }
+      // const decode: jwt.JwtPayload | null = jwt.decode(accessToken, {
+      //   complete: true,
+      // });
+      // const nowDate: number = new Date().getTime() / 1000;
+      // // 토큰 만료시간이 지났다면
+      // if (decode?.exp != undefined) {
+      //   if (decode.exp < nowDate) {
+      //     // 재발급 axios ㄱㄱ
+      //     // 리프레쉬 토큰 발급 서버 요청
+      //     // accessToken = refreshToken;
+      //   }
+      // }
     } else {
       // 토큰이 null이면
     }
