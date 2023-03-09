87f1cd2e8d11982c0c6fc698199d61bafb286eac
wndjf11 <wndjf11@naver.com>
2023-03-09 11:55:52 +0900

    Feat : 수정 require 를 import로

--- front-server/src/Components/Common/Api.tsx
+++ front-server/src/Components/Common/Api.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import jwt from "jsonwebtoken";
 
 const Api = axios.create({
   baseURL: "https://i8e102.p.ssafy.io",
@@ -11,27 +12,37 @@ Api.interceptors.request.use(
   // request할때 할 행동
   function (config) {
     console.log("컨피그", config);
-
     // 요청을 보내기 전 수행할 작업 = accessToken이 유효한지
-    const jwt = require("jsonwebtoken");
-    const crypto = require("crypto");
+
     // sessionStorage에서 accessToken을 가져온다.
     let accessToken: string | null = localStorage.getItem("accessToken");
-    // jwt를 decode 하여 payload를 추출한다.
-    const decode = jwt.decode(accessToken);
-    const nowDate: number = new Date().getTime() / 1000;
-    // 토큰 만료시간이 지났다면
-    if (decode.exp < nowDate) {
-      // 재발급 axios ㄱㄱ
-      // 리프레쉬 토큰 발급 서버 요청
-      // accessToken = refreshToken;
-    }
 
+    // jwt를 decode 하여 payload를 추출한다.
+    //토큰이 있으면
+    if (accessToken != null) {
+      // 유효성판단
+      console.log(accessToken);
+
+      const decode: jwt.JwtPayload | null = jwt.decode(accessToken, {
+        complete: true,
+      });
+      const nowDate: number = new Date().getTime() / 1000;
+      // 토큰 만료시간이 지났다면
+      if (decode?.exp != undefined) {
+        if (decode.exp < nowDate) {
+          // 재발급 axios ㄱㄱ
+          // 리프레쉬 토큰 발급 서버 요청
+          // accessToken = refreshToken;
+        }
+      }
+    } else {
+      // 토큰이 null이면
+    }
     return config;
   },
 
   function (err) {
-    // return Promise.reject(err);
+    return Promise.reject(err);
   },
 );
 
@@ -43,7 +54,7 @@ Api.interceptors.response.use(
     return config;
   },
   function (err) {
-    // return Promise.reject(err);
+    return Promise.reject(err);
   },
 );
 
