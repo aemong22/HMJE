1ae93fb0d198170c3a3f6ab37983a3e2bcc73fcf
wndjf11 <wndjf11@naver.com>
2023-03-10 10:32:23 +0900

    Feat : 추가 레이아웃 시작

--- front-server/src/Components/Dictionary/Dictionary.tsx
+++ front-server/src/Components/Dictionary/Dictionary.tsx
@@ -7,7 +7,18 @@ const Dictionary = () => {
     <>
       <div className="flex flex-col justify-between h-[100vh]">
         <Navbar />
-        <div className="h-[20rem]">사전나올곳</div>
+        <div className="flex flex-col justify-center items-center border-2 border-black mx-auto px-10 sm:px-10 md:px-16 lg:px-20">
+          <div className="flex flex-row justify-between py-10">
+            <div>사전[辭典]</div>
+            <input type="text" placeholder="검색" />
+          </div>
+          <div className="flex flex-row justify-between">
+            <div className="">좌</div>
+            <div className="">중</div>
+            <div className="">우</div>
+          </div>
+          <div className="py-10">하</div>
+        </div>
         <Footer />
       </div>
     </>
