a182339ca55996185c48f01adffd30be7734bd7e
eunjin <yej6642@naver.com>
2023-03-14 11:59:57 +0900

    Feat : 추가 sms security

--- back-server/src/main/java/com/server/back/config/SecurityConfig.java
+++ back-server/src/main/java/com/server/back/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
                 .antMatchers("**/login","/refresh/**","/user/join","/user/check/nickname", "/user/check/username")
-                .antMatchers("/sms/**","/sms/modify","**/sms/modify","/sms/send/**")
+                .antMatchers("/sms/**","**/sms/**","/sms/modify","**/sms/modify","/sms/send/**")
                 .antMatchers("/login/oauth2/code/naver","/user/oauth2/token/naver", "/api/user/oauth2/token/naver","/api/login/oauth2/code/naver")
                 .antMatchers("**/refresh/**","/api/user/auth/refresh/**","/user/auth/refresh/**")
                 .antMatchers("/v2/api-docs","/swagger**/**","/api/v2/**",
