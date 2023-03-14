194a57271e2b9026357e062486ae1b4495ff0269
wndjf11 <wndjf11@naver.com>
2023-03-14 09:24:50 +0900

    Rename : 변수 및 함수명 변경

--- front-server/src/Components/Join/Join.tsx
+++ front-server/src/Components/Join/Join.tsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Footer from "../Common/Footer";
 import Navbar from "../Common/Navbar";
 import API from "../Common/Api";
 import axios from "axios";
-
+var pattern2 = /[a-zA-Z]/; //영어
 const Join = () => {
   const navigate = useNavigate();
+
+  // 이름, 비밀번호, 비밀번호 확인, 별명, 폰번호, 인증번호
   const [Name, setName] = useState<string>("");
   const [Password, setPassword] = useState<string>("");
   const [PasswordCheck, setPasswordCheck] = useState<string>();
   const [Nickname, setNickname] = useState<string>("");
   const [Phonenum, setPhonenum] = useState<string>("");
   const [Authnum, setAuthnum] = useState<string>();
+
+  const [AmIHidden, setAmIHidden] = useState("hidden");
+
+  // 오류메세지 상태저장
+  const [passwordMessage, setPasswordMessage] = useState<string>("");
+  const [passwordConfirmMessage, setPasswordConfirmMessage] =
+    useState<string>("");
+
+  // 유효성 검사
+  const [isName, setIsName] = useState<boolean>(false);
+  const [isEmail, setIsEmail] = useState<boolean>(false);
+  const [isPassword, setIsPassword] = useState<boolean>(false);
+  const [isPasswordConfirm, setIsPasswordConfirm] = useState<boolean>(false);
 
   function ChangeName(event: any): void {
     console.log(event.target.value);
@@ -24,14 +39,16 @@ const Join = () => {
   };
   const ChangePasswordCheck = (event: any): void => {
     console.log(event.target.value);
-    console.log(event.target.value);
     setPasswordCheck(event.target.value);
-    SameCheck(event.tartget.value);
   };
   const ChangeNickname = (event: any): void => {
+    event.preventDefault();
+    event.preventDefault();
     console.log(event.target.value);
     setNickname(event.target.value);
+    // CheckEnglish();
   };
+
   const ChangePhonenum = (event: any): void => {
     console.log(event.target.value);
     setPhonenum(event.target.value);
@@ -96,6 +113,12 @@ const Join = () => {
     });
   };
 
+  const sampleTest = () => {
+    API.get(`/admin/user`).then((r) => {
+      console.log(r.data);
+    });
+  };
+
   const GoJoin = (): void => {
     // 회원가입axios
     console.log("닉네임", Nickname);
@@ -149,12 +172,6 @@ const Join = () => {
     [Password],
   );
 
-  const SameCheck = (test: any) => {
-    console.log("props넘어오는값", test);
-    console.log("Password", Password);
-  };
-
-  const [AmIHidden, setAmIHidden] = useState("hidden");
   const PhoneCheck = (): any => {
     // SendAuthnum(Phonenum);
     if (Phonenum.length === 11) {
@@ -206,9 +223,12 @@ const Join = () => {
 
                   <div
                     className="px-3 py-1 md:px-4 md:py-2 border-2 bg-[#BF9F91] text-[#FFFFFF]  rounded-lg font-medium"
-                    onClick={() => {
-                      CheckDuplication("Name");
-                    }}
+                    onClick={
+                      () => {
+                        CheckDuplication("Name");
+                      }
+                      // sampleTest
+                    }
                   >
                     중복확인
                   </div>
@@ -226,8 +246,10 @@ const Join = () => {
                 <div className="flex flex-row justify-between ">
                   <input
                     type="text"
+                    id="id"
                     className="min-w-[70%] px-3 py-1 md:px-4 md:py-2 border-2 border-[#A87E6E] rounded-lg font-medium placeholder:font-normal"
                     onChange={ChangeNickname}
+                    onKeyUp={chkCharCode}
                   />
                   <div
                     className="px-3 py-1 md:px-4 md:py-2 border-2 bg-[#BF9F91] text-[#FFFFFF]  rounded-lg font-medium"
@@ -255,12 +277,23 @@ const Join = () => {
                 <div className="text-[#A87C6E] font-extrabold text-base">
                   비밀번호확인
                 </div>
-                <div className="flex flex-row ">
+                <div className="flex flex-col ">
                   <input
                     type="text"
                     className="min-w-[100%] px-3 py-1 md:px-4 md:py-2 border-2 border-[#A87E6E] rounded-lg font-medium placeholder:font-normal"
-                    onChange={ChangePasswordCheck}
+                    onChange={onChangePasswordConfirm}
+                    title="비밀번호 확인"
                   />
+
+                  {PasswordCheck && PasswordCheck.length > 0 && (
+                    <span
+                      className={`message ${
+                        isPasswordConfirm ? "success" : "error"
+                      }`}
+                    >
+                      {passwordConfirmMessage}
+                    </span>
+                  )}
                 </div>
               </div>
               <div className="my-4">
@@ -275,7 +308,9 @@ const Join = () => {
                   />
                   <div
                     className="px-3 py-1 md:px-4 md:py-2 border-2 bg-[#BF9F91] text-[#FFFFFF]  rounded-lg font-medium"
-                    onClick={PhoneCheck}
+                    onClick={() => {
+                      SendAuthnum(Phonenum);
+                    }}
                   >
                     인증하기
                   </div>
@@ -302,13 +337,13 @@ const Join = () => {
                 </div>
               </div>
               <div className="w-full">
-                <div
-                  className="pt-3 cursor-pointer"
-                  onClick={() => {
-                    GoJoin();
-                  }}
-                >
-                  <div className="flex justify-center items-center h-[3.5rem] rounded-lg font-extrabold bg-[#F0ECE9] text-[#A87E6E]">
+                <div className="pt-3 cursor-pointer">
+                  <div
+                    className="flex justify-center items-center h-[3.5rem] rounded-lg font-extrabold bg-[#F0ECE9] text-[#A87E6E]"
+                    onClick={() => {
+                      GoJoin();
+                    }}
+                  >
                     <div>가입하기</div>
                   </div>
                 </div>
