80db55b98dede2f1f604449ce23eb273702f5217
KHminor <ghdals2020@gmail.com>
2023-03-14 14:17:39 +0900

    Feat: 수정 RTK

--- front-server/src/Store/api.ts
+++ front-server/src/Store/api.ts
@@ -1,33 +1,3 @@
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
-
-// Define a service using a base URL and expected endpoints
-// export const api = createApi({
-//   reducerPath: 'api',
-//   tagTypes: ['Api'],
-//   baseQuery: fetchBaseQuery({ baseUrl: 'https://hmje.net/api' }),
-//   endpoints: (builder) => ({
-//     getAdminUserList: builder.query({
-//       query: () => `/api/admin/user`,
-//       providesTags: (result, error, arg) => {
-//         return [{type: "Api"}]
-//       }
-//     }),
-//     // setCount: builder.mutation({
-//     //   query: ({name, value}) => {
-//     //     return {
-//     //       url: '',
-//     //       method: 'POST',
-//     //       body: {value}
-//     //     }
-//     //   },
-//     //   invalidatesTags: (result, error, arg) => [{ type: "Api", id: arg.name}]
-//     // })
-//   }),
-// })
-
-// export const {useGetAdminUserListQuery}:any = api;
-
-
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const accessToken:any = localStorage.getItem("accessToken")
 const refreshToken:any = localStorage.getItem("refreshToken")
