d840ffa2336732efcc1e68417c83f7bc78226335
김애림 <pinkpig_21@naver.com>
2023-03-15 11:30:12 +0900

    Feature : 수정 main

--- front-server/src/Components/Main/Main.tsx
+++ front-server/src/Components/Main/Main.tsx
@@ -9,6 +9,9 @@ import style from "./Main.module.css";
 
 
 function Main(): JSX.Element {
+  // const [getUserInfo, {data:userId}] = useGetUserMyinfoQuery();
+
+
   const [selectKeyWord, setSelectKeyWord] = useState<number>(0);
 
   // 뉴스 핵심 단어 List
